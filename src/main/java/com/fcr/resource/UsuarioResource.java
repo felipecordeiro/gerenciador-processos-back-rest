ab4e5c4e8968721e29e4b809ce7ba006cd5bd639
Felipe Cordeiro Ramos <felipecordeiro4@gmail.com>
2020-08-13 00:56:21 -0300

    novos endpoints UsuarioResource

--- src/main/java/com/fcr/resource/UsuarioResource.java
+++ src/main/java/com/fcr/resource/UsuarioResource.java
@@ -5,11 +5,16 @@ import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.fcr.modelo.enums.Funcao;
 import com.fcr.modelo.to.Usuario;
@@ -21,17 +26,61 @@ import com.fcr.modelo.to.Usuario;
 public class UsuarioResource {
 
 	private List<Usuario> usuarios = new ArrayList<>();
-	
+
 	public UsuarioResource() {
 
 		usuarios.add((new Usuario(1L, "administrador", "senha1", "João", Funcao.USUARIO_ADMINISTRADOR)));
 		usuarios.add((new Usuario(2L, "triador", "senha2", "Gabriela", Funcao.USUARIO_TRIADOR)));
 		usuarios.add((new Usuario(3L, "finalizador", "senha3", "Maria", Funcao.USUARIO_FINALIZADOR)));
-		
+
 	}
-	
+
 	@GET
-	public Response list() {
+	public Response listAll() {
 		return Response.ok(usuarios).build();
 	}
+
+	@GET
+	@Path("/{id}")
+	public Response getUsuarioById(@PathParam(value = "id") Long id) {
+		Usuario usuario = new Usuario();
+		usuario.setId(id);
+		return usuarios.contains(usuario) ? Response.ok(usuarios.get(usuarios.indexOf(usuario))).build()
+				: Response.status(Status.NOT_FOUND).build();
+	}
+
+	@POST
+	public Response save(Usuario usuario) {
+		long id = usuarios.get(usuarios.size() - 1).getId() + 1;
+		usuario.setId(id);
+		usuarios.add(usuario);
+		return Response.status(Status.CREATED).build();
+	}
+
+	@DELETE
+	@Path("/{id}")
+	public Response delete(@PathParam(value = "id") Long id) {
+		Usuario usuario = new Usuario();
+		usuario.setId(id);
+		if (usuarios.contains(usuario)) {
+			usuarios.remove(usuario);
+			return Response.status(Status.NO_CONTENT).build();
+		} else {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+	}
+
+	@PUT
+	public Response update(Usuario usuario) {
+		if (usuarios.contains(usuario)) {
+			usuarios.get(usuarios.indexOf(usuario)).setLogin(usuario.getLogin());
+			usuarios.get(usuarios.indexOf(usuario)).setNome(usuario.getNome());
+			usuarios.get(usuarios.indexOf(usuario)).setPassword(usuario.getPassword());
+			usuarios.get(usuarios.indexOf(usuario)).setFuncao(usuario.getFuncao());
+			return Response.status(Status.NO_CONTENT).build();
+		} else {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+	}
+
 }
